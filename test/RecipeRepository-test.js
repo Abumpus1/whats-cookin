787fa80c4bbb3ff7f98c7dce0672ddd59232b8c0
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-03-30 15:31:34 -0600

    Allow RecipeRepository to hold recipes

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -14,5 +14,9 @@ describe('Recipe Repository', () => {
     console.log(recipeRepo);
     expect(RecipeRepository).to.be.a('function');
   });
+  
+  it("should hold recipe data", () => {
+    expect(recipeRepo.recipes).to.deep.equal(recipeData);
+  });
 
 });
