6eadbcacb0a5de8a3503d4dcc3054adcfcd3fc14
Nick Orlov <ialwaysreply@gmail.com>
2022-03-30 19:06:41 -0700

    Modify index.html

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,9 @@ import './styles.css';
 import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import RecipeRepository from './classes/recipeRepository'
+import recipeData from './data/recipes'
 
+const activeRecipeRepo = new RecipeRepository(recipeData)
 
-
-console.log('Hello world');
+console.log(activeRecipeRepo);
