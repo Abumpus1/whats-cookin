8aefb759e56dc6fd8117e1e047c7457cf4d10d21
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-03-30 18:07:09 -0600

    Refactor Recipe

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,4 +1,5 @@
-import ingredientsData from '../data/ingredients'
+import ingredientsData from '../data/ingredients';
+
 class Recipe {
   constructor(recipe) {
     this.id = recipe.id;
@@ -8,11 +9,7 @@ class Recipe {
     this.name = recipe.name;
     this.tags = recipe.tags;
   }
-  //GOAL - array of strings that are ingredient names
-  //input - array of objs(ingredientsData)
-  //compare ids of ingredientsData.id and recipe.ingredients.id
-  //filter ingredientsData, then for each filtered obj, push obj.name to acc
-  //output - ["wheat flour", "bicarbonate of soda"]
+
   filterIngredients(ingredient){
     return ingredientsData.filter(dataIng => dataIng.id === ingredient.id);
   }
@@ -20,9 +17,7 @@ class Recipe {
   getIngredientNames() {
     return this.ingredients.reduce((acc, ingredient) => {
       let filteredIngs = this.filterIngredients(ingredient);
-      filteredIngs.forEach((filterIng) => {
-        acc.push(filterIng.name);
-      });
+      filteredIngs.forEach((filterIng) => acc.push(filterIng.name));
       return acc;
     },[]);
   }
@@ -31,22 +26,17 @@ class Recipe {
     return this.ingredients.reduce((acc, ingredient) => {
       let filteredIngs = this.filterIngredients(ingredient);
       filteredIngs.forEach((filterIng) => {
-        acc += (ingredient.quantity.amount * filterIng.estimatedCostInCents)
+        acc += (ingredient.quantity.amount * filterIng.estimatedCostInCents);
       });
       return acc;
     },0);
-  };
+  }
 
-    getRecipeDirections(){
-      return this.instructions.map((instruction) => {
-        return `Step ${instruction.number}: ${instruction.instruction}`
-      })
-    }
-
+  getRecipeDirections(){
+    return this.instructions.map((instruction) => {
+      return `Step ${instruction.number}: ${instruction.instruction}`;
+    });
+  }
 }
 
 export default Recipe;
-
-// Determine the names of ingredients needed
-// Get the cost of its ingredients
-// Return its directions / instructions
