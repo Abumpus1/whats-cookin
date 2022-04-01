69c3883c54b4c359647beebe26802f63abe179be
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-03-31 20:24:26 -0500

    Allow recipes to be displayed via DOM

--- src/scripts.js
+++ src/scripts.js
@@ -7,4 +7,33 @@ import recipeData from './data/recipes'
 
 const activeRecipeRepo = new RecipeRepository(recipeData)
 
-console.log(activeRecipeRepo);
+//QUERY SELECTORS//
+const recipesList = document.querySelector(".recipes-list");
+
+
+//EVENT LISTENERS//
+//function on page load that goes through recipes array and displays within HTML each recipe
+const displayAllRecipes = () => {
+  //loop through recipes data, for each recipe recipesList.innerHTML +=
+  recipesList.innerHTML = "";
+  activeRecipeRepo.recipes.forEach(recipe => {
+    // let recipeOne = document.createElement("div");
+    // recipeOne.className = "recipe";
+    // recipesList.appendChild(recipeOne);
+    recipesList.innerHTML += `
+    <section class="recipe">
+      <div>
+        <img src="${recipe.image}" class="recipe-image">
+      </div>
+      <div>
+        <h3>${recipe.name}</h3>
+      </div>
+    </section>
+    `
+    //grab each recipe from our activeRecipeRepo
+    //once we got it, want to create a new element and assign a class to then add to recipes list section
+    //innerHTML
+  })
+}
+
+window.addEventListener('load', displayAllRecipes)
