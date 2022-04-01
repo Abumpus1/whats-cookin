a5b5c27189b414b5b660d78f80988cc2bd085e54
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-04-01 17:39:59 -0600

    Add Ingredient class

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -4,13 +4,24 @@ import sampleIngredients from '../src/data/sample-ingredients';
 
 describe('Ingredient', () => {
   let ingredient
+
   beforeEach(() => {
-    ingredient = new Ingredient(sampleIngredients[0])
+    ingredient = new Ingredient(sampleIngredients[0]);
   });
 
   it('Should be a function', () => {
     expect(Ingredient).to.be.a('function');
   });
+  
+  it('Should have an id', () => {
+    expect(ingredient.id).to.equal(20081);
+  });
 
+  it('Should have a name', () => {
+    expect(ingredient.name).to.equal('wheat flour');
+  });
 
+  it('Should have an estimated cost in cents', () => {
+    expect(ingredient.estimatedCostInCents).to.equal(142);
+  });
 });
