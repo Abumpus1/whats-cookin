343a05502a5711e9248ea7ccf4563eea2b6659d6
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-03-31 21:01:00 -0500

    Add all recipes event listener

--- src/scripts.js
+++ src/scripts.js
@@ -9,19 +9,23 @@ const activeRecipeRepo = new RecipeRepository(recipeData)
 
 //QUERY SELECTORS//
 const recipesList = document.querySelector(".recipes-list");
+const tagFilter = document.querySelector(".tag-filter");
+const allRecipesBox = document.querySelector(".all-recipes-box");
+const recipePage = document.querySelector(".recipe-page");
+//FUNCTIONS//
+const hide = (element => {
+  element.classList.add("hidden");
+});
 
+const show = (element => {
+  element.classList.remove("hidden")
+});
 
-//EVENT LISTENERS//
-//function on page load that goes through recipes array and displays within HTML each recipe
 const displayAllRecipes = () => {
-  //loop through recipes data, for each recipe recipesList.innerHTML +=
   recipesList.innerHTML = "";
   activeRecipeRepo.recipes.forEach(recipe => {
-    // let recipeOne = document.createElement("div");
-    // recipeOne.className = "recipe";
-    // recipesList.appendChild(recipeOne);
     recipesList.innerHTML += `
-    <section class="recipe">
+    <section class="recipe" id="${recipe.id}">
       <div>
         <img src="${recipe.image}" class="recipe-image">
       </div>
@@ -30,10 +34,19 @@ const displayAllRecipes = () => {
       </div>
     </section>
     `
-    //grab each recipe from our activeRecipeRepo
-    //once we got it, want to create a new element and assign a class to then add to recipes list section
-    //innerHTML
   })
 }
 
+const displayRecipeDetails = (event) => {
+  activeRecipeRepo.recipes.forEach(recipe => {
+    if(event.target.closest(".recipe").id === `${recipe.id}`){
+      console.log("IT WORKED");
+    }
+  });
+}
+
+//EVENT LISTENERS//
 window.addEventListener('load', displayAllRecipes)
+recipesList.addEventListener('click', (event) => {
+  displayRecipeDetails(event);
+})
