46c4f3c9f0f5c64bd240a5ce13dde11a5278087d
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-04-04 16:18:04 -0600

    Refactor

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,5 @@
 import './styles.css';
-// import apiCalls from './apiCalls';
 import { fetchedUserData, fetchedIngredientsData, fetchedRecipesData } from './apiCalls';
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
 import RecipeRepository from './classes/recipeRepository';
 
