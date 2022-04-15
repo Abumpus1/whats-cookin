706ac4ec21ae2a3a392e15c48e75cffdf501da4a
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-04-15 16:45:43 -0500

    Refactor fetch error handling, fix favorite button bug

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,20 +1,18 @@
 function fetchData(dataLocation) {
   return fetch(`http://localhost:3001/api/v1/${dataLocation}`)
- .then(response => response.json())
-  if (!response.ok) {
+ .then(response => {
+   if (!response.ok) {
    throw Error(response.statusText)
   }
   return response.json()
+  })
  .catch(err => displayErrorMessage(err));
 }
 
 
 const displayErrorMessage = (err) => {
   const errorBox = document.querySelector(".error-box");
-  if(err.message === '404 (Not Found)'){
-    errorBox.innerHTML = `<h3>Oops, something went wrong. Please check your internet connection or refresh the page.</h3>`
-  }
-  errorBox.innerText = ${err.message}
+  errorBox.innerHTML = `<h3>Oops, something went wrong. Please check your internet connection or refresh the page.</h3>`  
 }
 
 export {
